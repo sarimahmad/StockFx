88a3596abe62a0e30005f2f3dca40b739d3d71d7
AwaisAsh <awais.ashraf@ivylabtech.com>
2021-03-17 13:43:40 +0500

    Setting screen

--- StockFX/App/screens/Setting/SettingStyles.js
+++ StockFX/App/screens/Setting/SettingStyles.js
@@ -20,9 +20,39 @@ const SettingStyles = {
     fontSize: 12px;
     fontFamily: ${FONT.bold};
     color: ${WHITE.app};
+    marginBottom: 10px;
   `,
   ItemWrapper: styled.View`
-  
+    flexDirection: row;
+    align-items: center;
+    borderBottomWidth: 1px;
+    border-bottom-color: ${BLUE.drawerLine};
+    marginTop: 10px;
+  `,
+  ItemInnerLeft: styled.View`
+    flex: 0.7;
+    justify-content: center;
+  `,
+  ItemRightView: styled.View`
+    flex: 0.3;
+    justify-content: center;
+    alignItems: flex-end;
+  `,
+  OpenSans10Regular: styled.Text`
+    fontFamily: ${FONT.regular};
+    fontSize: 10px;
+    color: ${BLUE.light};
+  `,
+  OpenSans14Regular: styled.Text`
+    fontFamily: ${FONT.regular};
+    fontSize: 14px;
+    color: ${WHITE.off};
+    marginVertical: 10px;
+  `,
+  RightIcon: styled.Image`
+    height: 14px;
+    width: 14px;
+    resizeMode: contain;
   `,
 };
 
--- StockFX/App/screens/Setting/index.js
+++ StockFX/App/screens/Setting/index.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
+import {Switch} from 'react-native';
 import {connect} from 'react-redux';
-import {NavigationEvents} from 'react-navigation';
 
 import * as Actions from '../../redux/actions';
 import SettingStyles from './SettingStyles';
-import HeaderWithOptionBtn from '../../component/HeaderWithOptionBtn';
 import HeaderWithSearch from '../../component/HeaderWithSearch';
+import {BLACK, BLUE, YELLOW} from '../../helper/Color';
 
 class Setting extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      coinUpdate: false
+    }
+  }
+  
   render() {
     return (
       <SettingStyles.WrapperViewVertical>
@@ -18,9 +25,100 @@ class Setting extends Component {
         />
         <SettingStyles.SafeArea>
           <SettingStyles.SimpleView>
-            <SettingStyles.OpenSans12Bold>
-              General
-            </SettingStyles.OpenSans12Bold>
+            <SettingStyles.OpenSans12Bold>General</SettingStyles.OpenSans12Bold>
+            <SettingStyles.ItemWrapper>
+              <SettingStyles.ItemInnerLeft>
+                <SettingStyles.OpenSans10Regular>
+                  Language
+                </SettingStyles.OpenSans10Regular>
+                <SettingStyles.OpenSans14Regular>
+                  English
+                </SettingStyles.OpenSans14Regular>
+              </SettingStyles.ItemInnerLeft>
+              <SettingStyles.ItemRightView>
+                <SettingStyles.RightIcon
+                  source={require('../../assets/rightArrow.png')}
+                />
+              </SettingStyles.ItemRightView>
+            </SettingStyles.ItemWrapper>
+
+            <SettingStyles.ItemWrapper>
+              <SettingStyles.ItemInnerLeft>
+                <SettingStyles.OpenSans10Regular>
+                  Local Fiat Currency
+                </SettingStyles.OpenSans10Regular>
+                <SettingStyles.OpenSans14Regular>
+                  US Dollar (USD)
+                </SettingStyles.OpenSans14Regular>
+              </SettingStyles.ItemInnerLeft>
+              <SettingStyles.ItemRightView>
+                <SettingStyles.RightIcon
+                  source={require('../../assets/rightArrow.png')}
+                />
+              </SettingStyles.ItemRightView>
+            </SettingStyles.ItemWrapper>
+
+            <SettingStyles.ItemWrapper>
+              <SettingStyles.ItemInnerLeft>
+                <SettingStyles.OpenSans14Regular>
+                  Notification Options
+                </SettingStyles.OpenSans14Regular>
+              </SettingStyles.ItemInnerLeft>
+              <SettingStyles.ItemRightView>
+                <SettingStyles.RightIcon
+                  source={require('../../assets/rightArrow.png')}
+                />
+              </SettingStyles.ItemRightView>
+            </SettingStyles.ItemWrapper>
+
+            <SettingStyles.ItemWrapper>
+              <SettingStyles.ItemInnerLeft>
+                <SettingStyles.OpenSans14Regular>
+                  View Options
+                </SettingStyles.OpenSans14Regular>
+              </SettingStyles.ItemInnerLeft>
+              <SettingStyles.ItemRightView>
+                <SettingStyles.RightIcon
+                  source={require('../../assets/rightArrow.png')}
+                />
+              </SettingStyles.ItemRightView>
+            </SettingStyles.ItemWrapper>
+
+            <SettingStyles.ItemWrapper>
+              <SettingStyles.ItemInnerLeft>
+                <SettingStyles.OpenSans14Regular>
+                  Other Options
+                </SettingStyles.OpenSans14Regular>
+              </SettingStyles.ItemInnerLeft>
+              <SettingStyles.ItemRightView>
+                <SettingStyles.RightIcon
+                  source={require('../../assets/rightArrow.png')}
+                />
+              </SettingStyles.ItemRightView>
+            </SettingStyles.ItemWrapper>
+            
+            <SettingStyles.OpenSans12Bold style={{marginTop: 30}}>Security</SettingStyles.OpenSans12Bold>
+            <SettingStyles.ItemWrapper>
+              <SettingStyles.ItemInnerLeft>
+                <SettingStyles.OpenSans14Regular>
+                  Passcode Lock
+                </SettingStyles.OpenSans14Regular>
+              </SettingStyles.ItemInnerLeft>
+              <SettingStyles.ItemRightView>
+                <Switch
+                  value={this.state.coinUpdate}
+                  trackColor={{
+                    false: BLACK.textColorDark,
+                    true: YELLOW.app,
+                  }}
+                  thumbColor={
+                    this.state.coinUpdate ? BLUE.drawerLine : BLUE.light
+                  }
+                  ios_backgroundColor={BLACK.textColorDark}
+                  onValueChange={(coinUpdate) => this.setState({coinUpdate})}
+                />
+              </SettingStyles.ItemRightView>
+            </SettingStyles.ItemWrapper>
           </SettingStyles.SimpleView>
         </SettingStyles.SafeArea>
       </SettingStyles.WrapperViewVertical>
