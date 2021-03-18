e5c62774b536fc4d0a12fc122f09a5ffc6f02d7e
AwaisAsh <awais.ashraf@ivylabtech.com>
2021-03-18 12:10:56 +0500

    Contact US

--- StockFX/App/screens/ContactUs/ContactUsStyles.js
+++ StockFX/App/screens/ContactUs/ContactUsStyles.js
@@ -1,17 +1,76 @@
 /* eslint-disable prettier/prettier */
 import styled from 'styled-components/native';
-import {BLUE, GREEN, WHITE} from '../../helper/Color';
+import {BLUE, WHITE} from '../../helper/Color';
+import {FONT, SCREEN} from '../../helper/Constant';
 
 const ContactUsStyles = {
   WrapperViewVertical: styled.View`
     flex: 1;
-    justifyContent: center;
-    backgroundColor: ${BLUE.app};
+    backgroundColor: ${BLUE.background};
   `,
-  TextScreenTextScreen: styled.Text`
+  SafeView: styled.SafeAreaView`
+    flex: 1;
+  `,
+  Scroll: styled.ScrollView`
+    flex: 1;
+  `,
+  SimpleView: styled.View`
+    width: ${SCREEN.width - 60}px;
+    align-self: center;
+    margin-top: 15px
+  `,
+  OpenSans12Bold: styled.Text`
+    font-size: 12px;
+    font-family: ${FONT.bold};
+    color: ${WHITE.app};
+    margin-bottom: 10px;
+  `,
+  ItemWrapper: styled.View`
+    flex-direction: row;
+    align-items: center;
+    border-bottom-width: 1px;
+    border-bottom-color: ${BLUE.drawerLine};
+    margin-top: 10px;
+  `,
+  ItemInnerLeft: styled.View`
+    flex: 1;
+    justify-content: center;
+  `,OpenSans10Regular: styled.Text`
+    font-family: ${FONT.regular};
+    font-size: 10px;
+    color: ${BLUE.light};
+  `,
+  TextInput: styled.TextInput`
+    font-family: ${FONT.regular};
+    font-size: 14px;
+    color: ${WHITE.off};
+    marginVertical: 10px;
+    paddingHorizontal: 5px;
+  `,
+  TextInputMessage: styled.TextInput`
+    font-family: ${FONT.regular};
+    font-size: 14px;
+    color: ${WHITE.off};
+    marginVertical: 10px;
+    borderWidth: 1px;
+    borderColor: ${BLUE.light};
+    borderRadius: 10px;
+    padding: 10px;
+    minHeight: 200px;
+  `,
+  SendBtn: styled.TouchableHighlight`
+    height: 40px;
+    width: ${SCREEN.width - 60}px;
+    marginTop: 10px;
+    border-radius: 10px;
+    justify-content: center;
+    align-items: center;
+    background-color: ${BLUE.light};
+  `,
+  SendBtnText: styled.Text`
     fontSize: 16px;
-    color: ${WHITE.dark};
-    alignSelf: center;
+    fontFamily: ${FONT.bold};
+    color: ${WHITE.off};
   `,
 };
 
--- StockFX/App/screens/ContactUs/index.js
+++ StockFX/App/screens/ContactUs/index.js
@@ -1,31 +1,88 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import {NavigationEvents} from 'react-navigation';
 
 import * as Actions from '../../redux/actions';
 import ContactUsStyles from './ContactUsStyles';
+import {BLUE, WHITE} from '../../helper/Color';
+import HeaderWithOptionBtn from '../../component/HeaderWithOptionBtn';
 
 class ContactUs extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      message: '',
+      email: '',
+    };
+  }
   render() {
     return (
       <ContactUsStyles.WrapperViewVertical>
-        <NavigationEvents
-          onDidFocus={(payload) => {
-            setTimeout(() => {
-            }, 700);
-          }}
+        <HeaderWithOptionBtn
+          headerText={'ContactUs'}
+          backColor={BLUE.app}
+          leftIcon={require('../../assets/menu_btn.png')}
+          leftPress={() => this.props.navigation.openDrawer()}
         />
-        <ContactUsStyles.TextScreenTextScreen>
-          notification
-        </ContactUsStyles.TextScreenTextScreen>
+        <ContactUsStyles.SafeView>
+          <ContactUsStyles.Scroll bounces={false}>
+            <ContactUsStyles.SimpleView>
+              <ContactUsStyles.ItemWrapper>
+                <ContactUsStyles.ItemInnerLeft>
+                  <ContactUsStyles.OpenSans10Regular>
+                    Your name
+                  </ContactUsStyles.OpenSans10Regular>
+                  <ContactUsStyles.TextInput
+                    value={this.state.name}
+                    placeholder={'Ash'}
+                    placeholderTextColor={WHITE.off}
+                    onChangeText={(name) => this.setState({name})}
+                  />
+                </ContactUsStyles.ItemInnerLeft>
+              </ContactUsStyles.ItemWrapper>
+
+              <ContactUsStyles.ItemWrapper>
+                <ContactUsStyles.ItemInnerLeft>
+                  <ContactUsStyles.OpenSans10Regular>
+                    Your Email
+                  </ContactUsStyles.OpenSans10Regular>
+                  <ContactUsStyles.TextInput
+                    value={this.state.email}
+                    placeholder={'Ash@taker.com'}
+                    placeholderTextColor={WHITE.off}
+                    onChangeText={(email) => this.setState({email})}
+                  />
+                </ContactUsStyles.ItemInnerLeft>
+              </ContactUsStyles.ItemWrapper>
+
+              <ContactUsStyles.ItemWrapper style={{borderBottomWidth: 0}}>
+                <ContactUsStyles.ItemInnerLeft>
+                  <ContactUsStyles.OpenSans10Regular>
+                    Message
+                  </ContactUsStyles.OpenSans10Regular>
+                  <ContactUsStyles.TextInputMessage
+                    multiline={true}
+                    value={this.state.message}
+                    placeholder={'Type the message here'}
+                    placeholderTextColor={WHITE.off}
+                    onChangeText={(message) => this.setState({message})}
+                  />
+                </ContactUsStyles.ItemInnerLeft>
+              </ContactUsStyles.ItemWrapper>
+              <ContactUsStyles.SendBtn>
+                <ContactUsStyles.SendBtnText>Send</ContactUsStyles.SendBtnText>
+              </ContactUsStyles.SendBtn>
+            </ContactUsStyles.SimpleView>
+          </ContactUsStyles.Scroll>
+        </ContactUsStyles.SafeView>
       </ContactUsStyles.WrapperViewVertical>
     );
   }
 }
 
-function mapStateToProps(state, props) {
+function mapStateToProps(state) {
   return {
     userDetail: state.user.userDetail,
     userToken: state.user.userToken,
